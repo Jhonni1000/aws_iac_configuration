2e00fba1f8b261a93df4f8cd1eee1a580dd8147c
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-08-18 01:18:21 +0200

    test

--- src/terraform/sc.tf
+++ src/terraform/sc.tf
@@ -6,7 +6,7 @@ resource "aws_servicecatalog_product" "ec2_product" {
   provisioning_artifact_parameters {
     name         = "v1"
     description  = "Initial version"
-    template_url = "https://servicecatalog-templates-12345/ec2_product_v1.yml.tpl"
+    template_url = "https://servicecatalog-templates-12345.s3.eu-north-1.amazonaws.com/ec2_product_v1.yml"
     type        = "CLOUD_FORMATION_TEMPLATE"
   }
 
--- src/terraform/sc_s3.tf
+++ src/terraform/sc_s3.tf
@@ -4,9 +4,30 @@ resource "aws_s3_bucket" "cfn_bucket" {
 
 resource "aws_s3_bucket_object" "ec2_template" {
   bucket = aws_s3_bucket.cfn_bucket.id
-  key    = "ec2_product_v1.yml.tpl"
+  key    = "ec2_product_v1.yml"
   acl    = "private"
 
-  content = templatefile("${path.module}/scripts/ec2_product_v1.yml.tpl", { ami_id = data.aws_ami.ami_latest.id })
+  content = templatefile("${path.module}/scripts/ec2_product_v1.yml.tpl", {
+    ami_id = data.aws_ami.ami_latest.id
+  })
+}
 
+data "aws_iam_policy_document" "s3_policy" {
+  statement {
+    principals {
+      type        = "Service"
+      identifiers = ["servicecatalog.amazonaws.com"]
+    }
+
+    actions = ["s3:GetObject"]
+
+    resources = [
+      "${aws_s3_bucket.cfn_bucket.arn}/*" 
+    ]
+  }
+}
+
+resource "aws_s3_bucket_policy" "s3_policy" {
+  bucket = aws_s3_bucket.cfn_bucket.id
+  policy = data.aws_iam_policy_document.s3_policy.json
 }
