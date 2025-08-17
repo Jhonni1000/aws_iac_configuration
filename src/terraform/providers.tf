8da5bcd4a44d55467d82146aa98637eaf5bb6388
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-08-17 23:12:53 +0200

    feat: make bucket name environment specific

--- src/terraform/providers.tf
+++ src/terraform/providers.tf
@@ -11,10 +11,3 @@ provider "aws" {
     region = "eu-north-1"
 }
 
-terraform {
-  backend "s3" {
-    bucket = "aws-iac-config-19 "
-    key    = "dev/terraform.tfstate"
-    region = "eu-north-1"
-  }
-}
