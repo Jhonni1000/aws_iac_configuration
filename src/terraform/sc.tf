416e51fe76b3334664cc1ddfdeb7f93c503a79ca
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-08-18 00:10:54 +0200

    fix: sc error

--- src/terraform/sc.tf
+++ src/terraform/sc.tf
@@ -7,5 +7,6 @@ resource "aws_servicecatalog_product" "ec2_product" {
     name         = "v1"
     description  = "Initial version"
     template_url = "https://servicecatalog-templates-12345/ec2_product_v1.yml.tpl"
+    type        = "CLOUD_FORMATION_TEMPLATE"
   }
 }
