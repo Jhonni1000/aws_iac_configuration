5b22f15132c6f372a78e462791068d3aa39deee6
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-08-18 00:55:58 +0200

    fix: typo

--- src/terraform/sc_s3.tf
+++ src/terraform/sc_s3.tf
@@ -5,7 +5,7 @@ resource "aws_s3_bucket" "cfn_bucket" {
 
 resource "aws_s3_bucket_object" "ec2_template" {
   bucket = aws_s3_bucket.cfn_bucket.id
-  key    = "ec2-product.yml"
+  key    = "ec2_product_v1.yml.tpl"
   acl    = "private"
 
   content = templatefile("${path.module}/scripts/ec2_product_v1.yml.tpl", { ami_id = data.aws_ami.ami_latest.id })
