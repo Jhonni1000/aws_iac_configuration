c9005c42e1d548b3af7090143596a63fcc2b22db
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-08-19 15:42:32 +0200

    test

--- src/terraform/sc.tf
+++ src/terraform/sc.tf
@@ -23,7 +23,7 @@ resource "aws_servicecatalog_portfolio" "ec2_product" {
 resource "aws_servicecatalog_principal_portfolio_association" "ec2_product" {
   portfolio_id  = aws_servicecatalog_portfolio.ec2_product.id
   principal_arn = "arn:aws:iam::738605694254:role/aws-reserved/sso.amazonaws.com/eu-north-1/AWSReservedSSO_AdministratorAccess_ae92e0dac5f28572"
-  principal_type = "ROLE"
+  principal_type = "IAM_PATTERN"
 }
 
 resource "aws_servicecatalog_product_portfolio_association" "portfolio_association" {
