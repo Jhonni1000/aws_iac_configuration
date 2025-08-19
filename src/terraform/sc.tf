aca4a78d63316b56c02cad73e1c1b03306551284
opaki_ors <opaki@orsted.com>
2025-08-19 15:25:36 +0200

    feat: cretae launch role for sc

--- src/terraform/sc.tf
+++ src/terraform/sc.tf
@@ -13,14 +13,30 @@ resource "aws_servicecatalog_product" "ec2_product" {
   depends_on = [ aws_s3_bucket_object.ec2_template ]
 }
 
+
 resource "aws_servicecatalog_portfolio" "ec2_product" {
   name        = "EC2-Portfolio"
   description = "Portfolio for EC2 products"
   provider_name = "OPAKI"
 }
 
-resource "aws_servicecatalog_principal_portfolio_association" "example" {
+resource "aws_servicecatalog_principal_portfolio_association" "ec2_product" {
   portfolio_id  = aws_servicecatalog_portfolio.ec2_product.id
   principal_arn = "arn:aws:iam::738605694254:role/aws-reserved/sso.amazonaws.com/eu-north-1/AWSReservedSSO_AdministratorAccess_ae92e0dac5f28572"
-  principal_type = "IAM"
+  principal_type = "ROLE"
+}
+
+resource "aws_servicecatalog_product_portfolio_association" "portfolio_association" {
+  portfolio_id = aws_servicecatalog_portfolio.ec2_product.id
+  product_id = aws_servicecatalog_product.ec2_product.id
+}
+
+resource "aws_servicecatalog_constraint" "launch_role" {
+  description = "Launch Role Constraints"
+  product_id = aws_servicecatalog_product.ec2_product.id
+  type = "LAUNCH"
+  portfolio_id = aws_servicecatalog_portfolio.ec2_product.id
+  parameters = jsonencode({
+    "LocalRoleName": "arn:aws:iam::738605694254:role/sc_launch_role"
+  })
 }
