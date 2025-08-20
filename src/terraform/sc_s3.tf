ea775163d344cdf605abac5e56dbd5928517399a
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2025-08-20 14:25:12 +0200

    fix: separate inital version

--- src/terraform/sc_s3.tf
+++ src/terraform/sc_s3.tf
@@ -13,6 +13,18 @@ resource "aws_s3_bucket_object" "ec2_template" {
   for_each = local.ec2_product_versions
 
   bucket = aws_s3_bucket.cfn_bucket.id
+  key    = "ec2/initial_version/ec2_product.yml"
+  acl    = "private"
+
+  content = templatefile("${path.module}/scripts/ec2_product_initial_verision.yml.tpl", {
+    ami_id = data.aws_ami.ami_latest.id
+  })
+}
+
+resource "aws_s3_bucket_object" "ec2_template" {
+  for_each = local.ec2_product_versions
+
+  bucket = aws_s3_bucket.cfn_bucket.id
   key    = "ec2/${each.key}/ec2_product.yml"
   acl    = "private"
 
