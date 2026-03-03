7c03131c68d3276f82157fadd7d91028543b04e6
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2026-03-03 01:55:20 +0100

    fix: wrong file path

--- src/terraform/sc_s3.tf
+++ src/terraform/sc_s3.tf
@@ -61,7 +61,7 @@ module "ec2_service_catalog_product" {
   template_vars = {}
   initial_template_file = "${path.module}/scripts/ec2_product_initial_version.yml.tpl"
   ec2_product_versions = {
-    v1 = "ec2_product_v1.yml.tpl",
-    v2 = "ec2_product_v2.yml.tpl"
+    v1 = "${path.module}/scripts/ec2_product_v1.yml.tpl",
+    v2 = "${path.module}/scripts/ec2_product_v2.yml.tpl"
   }
 }
