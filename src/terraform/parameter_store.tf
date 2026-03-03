4be1af2208de4635d973758196fac530f73cc409
Jhonni1000 <akintoyeopeyemi5@gmail.com>
2026-03-03 02:38:28 +0100

    test

--- src/terraform/parameter_store.tf
+++ src/terraform/parameter_store.tf
@@ -3,5 +3,5 @@ resource "aws_ssm_parameter" "ec2_ami" {
     type = "String"
     description = "Packer EC2 Ubuntu AMI"
     overwrite = true
-    value = data.aws_ami.packer_ami_id.id
+    value = data.aws_ami.packer_ami.id
 }
